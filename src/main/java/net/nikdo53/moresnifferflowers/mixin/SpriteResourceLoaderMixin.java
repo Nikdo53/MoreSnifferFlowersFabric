47f317faf069d6d9ec07e99653ad24331b197a43
Nikdo53 <usivatel@gmail.com>
2025-03-19 16:01:15 +0100

    MSF 1.5.3

--- src/main/java/net/nikdo53/moresnifferflowers/mixin/SpriteResourceLoaderMixin.java
+++ src/main/java/net/nikdo53/moresnifferflowers/mixin/SpriteResourceLoaderMixin.java
@@ -3,14 +3,13 @@ package net.nikdo53.moresnifferflowers.mixin;
 
 import com.google.common.collect.ImmutableList;
 import net.nikdo53.moresnifferflowers.MoreSnifferFlowers;
+import net.nikdo53.moresnifferflowers.mixin.accessor.PalettedPermutationsAccessor;
+import net.nikdo53.moresnifferflowers.mixin.accessor.SpriteResourceLoaderAccessor;
 import net.minecraft.client.renderer.texture.atlas.SpriteResourceLoader;
 import net.minecraft.client.renderer.texture.atlas.SpriteSource;
-import net.minecraft.client.renderer.texture.atlas.sources.PalettedPermutations;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -24,9 +23,10 @@ public abstract class SpriteResourceLoaderMixin {
             at = @At("RETURN"))
     private static void moresnifferflowers$load(ResourceManager resourceManager, ResourceLocation location, CallbackInfoReturnable<SpriteResourceLoader> cir) {
         final List<String> trims = List.of("aroma", "carnage", "tater", "nether_wart", "carotene", "grain", "beat");
-        
-        if (location.getPath().equals("armor_trims")) {
-            for (SpriteSource source : ((SpriteResourceLoaderMixin) (Object) cir.getReturnValue()).getSources()) {
+
+        if (location.equals(new ResourceLocation("minecraft", "armor_trims"))) {
+            SpriteResourceLoader ret = cir.getReturnValue();
+            for (SpriteSource source : ((SpriteResourceLoaderAccessor) ret).getSources()) {
                 if(source instanceof PalettedPermutationsAccessor permutations) {
                     for (String trim : trims) {
                         ResourceLocation trimLocation = MoreSnifferFlowers.loc("trims/models/armor/" + trim);
@@ -36,22 +36,5 @@ public abstract class SpriteResourceLoaderMixin {
                 }
             }
         }
-    }
-
-    @Accessor("sources")
-    abstract List<SpriteSource> getSources();
-
-    @Mixin(PalettedPermutations.class)
-    private interface PalettedPermutationsAccessor {
-
-        @Accessor
-        List<ResourceLocation> getTextures();
-
-        @Accessor("textures")
-        @Mutable
-        void setTextures(List<ResourceLocation> value);
-
-        @Accessor
-        ResourceLocation getPaletteKey();
     }
 }
